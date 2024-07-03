d063490f234bfc5b9baa4a10b80ed664ba7800e0
wesllen dhenmyz brito ferreira <156432084+wdhenmyz@users.noreply.github.com>
2024-07-03 10:31:43 -0300

    calculadora básica

--- calculadora_basica/main.js
+++ calculadora_basica/main.js
@@ -1,17 +1,18 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu, shell} = require('electron')
 
 let mainWindow = null;
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
         width: 470,
         height: 620,
-        autoHideMenuBar: true,
-        //frame: false,
+        icon: './app/icone.ico'
+        /*autoHideMenuBar: true,
+        frame: false,
         webPreferences:{
             preload:`${__dirname}/preload.js`
-        }
+        }*/
     });
-    //Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template))
     mainWindow.loadFile('app/calculadora.html')
 });
 
@@ -19,19 +20,8 @@ app.on("window-all-closed", () => {
     app.quit();
 });
 
-
-/*const janelasobre = ()=> {
-    const sobre = new BrowserWindow({
-        width: 350,
-        height: 320,
-        resizable: false,
-        autoHideMenuBar: true,
-    });
-    sobre.loadFile('app/sobre.html')
-}
-
 const template = [
-    {label:'arquivo',
+    /*{label:'arquivo',
         submenu:[
             {label: 'sair', click: ()=> app.quit(), accelerator: 'Alt+f4'} //definindo um evento click para fechar o app, definindo alt+f4 como atalho
         ]
@@ -45,12 +35,10 @@ const template = [
             {label: '-', role: 'zoomout'}, //reduzir zoom
             {label: 'zoom padrão', role: 'resetZoom'} //restaurar zoom ao inicial
         ]
-    },
+    },*/
     {label: 'ajuda', 
         submenu: [
-            {label: 'documentação', click: ()=> shell.openExternal('https://onsenacrn-my.sharepoint.com/:b:/g/personal/wesllen52872276_edum_rn_senac_br/Eb-xiq60jHBLpSq8aSbozO4BxdkyhgKyoitM1pxC86M8zQ?e=IyNsCl')}, //chama um evento para abrir um link externo
-            {type: 'separator'},
-            {label: 'sobre', click: ()=> janelasobre()}
+            {label: 'documentação', click: ()=> shell.openExternal('https://onsenacrn-my.sharepoint.com/:b:/g/personal/wesllen52872276_edum_rn_senac_br/EdoD9QZKJFpLpfsPFRqNR5cBqj25RYQ6iSSBYkPgRHDJAQ?e=IzGoo9')}, //chama um evento para abrir um link externo
         ]      
     }
-]*/
+]
