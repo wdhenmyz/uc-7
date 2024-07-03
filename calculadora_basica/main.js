54c9e44f8a530ef7feefe51a88e1b0d51e4c7975
wesllen dhenmyz brito ferreira <156432084+wdhenmyz@users.noreply.github.com>
2024-07-03 08:43:16 -0300

    calculadora básica

---
+++ calculadora_basica/main.js
@@ -0,0 +1,58 @@
+const {app, BrowserWindow, Menu, shell} = require('electron')
+
+let mainWindow = null;
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 470,
+        height: 710,
+        //autoHideMenuBar: true,
+        //frame: false,
+        icon: './app/assets/icon/pngtree-vector-bar-chart-icon-png-image_313722.ico',
+        webPreferences:{
+            preload:`${__dirname}/preload.js`
+        }
+    });
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+    mainWindow.loadFile('app/calcular_media.html')
+});
+
+app.on("window-all-closed", () => {
+    app.quit();
+});
+
+const janelasobre = ()=> {
+    const sobre = new BrowserWindow({
+        width: 350,
+        height: 320,
+        resizable: false,
+        autoHideMenuBar: true,
+        icon: './app/assets/icon/pngtree-vector-bar-chart-icon-png-image_313722.ico'
+    });
+    sobre.loadFile('app/sobre.html')
+}
+
+
+const template = [
+    {label:'arquivo',
+        submenu:[
+            {label: 'sair', click: ()=> app.quit(), accelerator: 'Alt+f4'} //definindo um evento click para fechar o app, definindo alt+f4 como atalho
+        ]
+    },
+    {label:'exibir',
+        submenu: [
+            {label: 'recarregar', role: 'reload'}, //função para recarregar a página
+            {label: 'ferramentas do desenvolvedor', role: 'toggleDevTools'}, //abre ferramentas avançadas
+            {type: 'separator'}, //separa linhas
+            {label: '+', role: 'zoomin'}, //aplicar zoom
+            {label: '-', role: 'zoomout'}, //reduzir zoom
+            {label: 'zoom padrão', role: 'resetZoom'} //restaurar zoom ao inicial
+        ]
+    },
+    {label: 'ajuda', 
+        submenu: [
+            {label: 'documentação', click: ()=> shell.openExternal('https://onsenacrn-my.sharepoint.com/:b:/g/personal/wesllen52872276_edum_rn_senac_br/Eb-xiq60jHBLpSq8aSbozO4BxdkyhgKyoitM1pxC86M8zQ?e=IyNsCl')}, //chama um evento para abrir um link externo
+            {type: 'separator'},
+            {label: 'sobre', click: ()=> janelasobre()}
+        ]      
+    }
+]
