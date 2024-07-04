905eba87270d69bd9235c2e34ed8b4ba5c77f935
wesllen dhenmyz brito ferreira <156432084+wdhenmyz@users.noreply.github.com>
2024-07-04 09:48:50 -0300

    calculadora básica

--- calculadora_basica/main.js
+++ calculadora_basica/main.js
@@ -25,17 +25,17 @@ const template = [
         submenu:[
             {label: 'sair', click: ()=> app.quit(), accelerator: 'Alt+f4'} //definindo um evento click para fechar o app, definindo alt+f4 como atalho
         ]
-    },
+    },*/
     {label:'exibir',
         submenu: [
             {label: 'recarregar', role: 'reload'}, //função para recarregar a página
-            {label: 'ferramentas do desenvolvedor', role: 'toggleDevTools'}, //abre ferramentas avançadas
+            /*{label: 'ferramentas do desenvolvedor', role: 'toggleDevTools'}, //abre ferramentas avançadas
             {type: 'separator'}, //separa linhas
             {label: '+', role: 'zoomin'}, //aplicar zoom
             {label: '-', role: 'zoomout'}, //reduzir zoom
-            {label: 'zoom padrão', role: 'resetZoom'} //restaurar zoom ao inicial
+            {label: 'zoom padrão', role: 'resetZoom'} //restaurar zoom ao inicial*/
         ]
-    },*/
+    },
     {label: 'ajuda', 
         submenu: [
             {label: 'documentação', click: ()=> shell.openExternal('https://onsenacrn-my.sharepoint.com/:b:/g/personal/wesllen52872276_edum_rn_senac_br/EdoD9QZKJFpLpfsPFRqNR5cBqj25RYQ6iSSBYkPgRHDJAQ?e=IzGoo9')}, //chama um evento para abrir um link externo
