c0c8e7dc68c59c02992f4ebbeb87c5fa95ad9e69
wesllen dhenmyz brito ferreira <156432084+wdhenmyz@users.noreply.github.com>
2024-07-08 10:58:02 -0300

    sistema de estacionamento

---
+++ sistema_estacionamento/main.js
@@ -0,0 +1,39 @@
+const {app, BrowserWindow, Menu} = require('electron')
+
+let mainWindow = null;
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 950,
+        height: 580,
+        icon: 'app/assets/css/icone.ico'
+        /*autoHideMenuBar: true,
+        frame: false,
+        webPreferences:{
+            preload:`${__dirname}/preload.js`
+        }*/
+    });
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+    mainWindow.loadFile('app/index.html')
+});
+
+app.on("window-all-closed", () => {
+    app.quit();
+});
+
+const template = [
+    {label:'arquivo',
+        submenu:[
+            {label: 'sair', click: ()=> app.quit(), accelerator: 'Alt+f4'} //definindo um evento click para fechar o app, definindo alt+f4 como atalho
+        ]
+    },
+    {label:'exibir',
+        submenu: [
+            {label: 'recarregar', role: 'reload'}, //função para recarregar a página
+            {label: 'ferramentas do desenvolvedor', role: 'toggleDevTools'}, //abre ferramentas avançadas
+            {type: 'separator'}, //separa linhas
+            {label: '+', role: 'zoomin'}, //aplicar zoom
+            {label: '-', role: 'zoomout'}, //reduzir zoom
+            {label: 'zoom padrão', role: 'resetZoom'} //restaurar zoom ao inicial
+        ]
+    },
+]
