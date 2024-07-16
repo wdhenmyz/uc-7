163c4cc52a8e6323771866fec69f5c5557cc2b78
wesllen dhenmyz brito ferreira <156432084+wdhenmyz@users.noreply.github.com>
2024-07-16 10:40:13 -0300

    jogo da memoria

--- jogo_da_memoria/app/script.js
+++ jogo_da_memoria/app/script.js
@@ -1,1 +1,30 @@
-c
+document.getElementById('reset').addEventListener('click', () => {
+    window.location.reload();
+})
+
+// This function shuffles the divs with the class "card"
+function shuffleCards() {
+    const cards = document.querySelectorAll(".card");
+    const shuffledCards = Array.from(cards).sort(() => Math.random() - 0.5);
+    const boards = document.querySelectorAll(".board");
+
+    for (let i = 0; i < boards.length; i++) {
+        const board = boards[i];
+        board.innerHTML = "";
+
+        // Delay adding the animation class to see the shuffle effect
+        setTimeout(() => {
+            board.classList.add("shuffle-animation");
+            shuffledCards.slice(i * 4, (i + 1) * 4).forEach(card => board.appendChild(card));
+        }, 200* i); // Adjust the delay as needed
+    }
+}
+
+// This function initializes the game
+function init() {
+    // Add a click event listener to the button with the id "start"
+    document.getElementById("start").addEventListener("click", shuffleCards);
+}
+
+init();
+
