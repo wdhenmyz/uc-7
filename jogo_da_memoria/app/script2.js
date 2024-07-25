5d18cc5b5cc047a571a669eb3a9b1d74c623499c
wesllen dhenmyz brito ferreira <156432084+wdhenmyz@users.noreply.github.com>
2024-07-25 08:42:47 -0300

    jogo da memoria

--- jogo_da_memoria/app/script2.js
+++ jogo_da_memoria/app/script2.js
@@ -2,10 +2,7 @@ const cards = document.querySelectorAll(".card");
 
 // check if cards match
 function check() {
-    if (cards[0].innerHTML === cards[1].innerHTML) {
-        cards[0].classList.add("match");
-        cards[1].classList.add("match");
-    }
+    
 }
 
     
