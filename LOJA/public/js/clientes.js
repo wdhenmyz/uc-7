43676c19fde7956db078adf3eca54459cb977f10
wesllen dhenmyz brito ferreira <156432084+wdhenmyz@users.noreply.github.com>
2024-07-25 11:39:35 -0300

    LOJA

---
+++ LOJA/public/js/clientes.js
@@ -0,0 +1,82 @@
+// garantir que o DOM esteja carregado, antes de carregar o conteúdo
+document.addEventListener("DOMContentLoaded", () => {
+    carregarClientes();
+    addRowToTable();
+
+    // carrega o formulário
+    const form = document.getElementById('cliente');
+    // cria uma função para cadastrar o cliente
+    form.addEventListener('submit', async(e) => {
+        e.preventDefault();
+        carregarClientes();
+    })
+
+    // carrega o formulário
+    const procurar = document.getElementById('procurar');
+    // cria uma função para procurar o cliente
+    procurar.addEventListener('submit', async(e) => {
+        e.preventDefault();
+        addRowToTable();
+    })
+});
+
+const cliente = document.getElementById("cliente");
+
+async function addRowToTable(e) {
+    // previne o comportamento padrão do formulário
+    e.preventDefault();
+
+    // pega os dados do formulário
+    const nome = document.getElementById("nome").value;
+    const nascimento = document.getElementById("data_nascimento").value;
+    const celular = document.getElementById("celular").value;
+    const endereco = document.getElementById("endereco").value;
+    const sexo = document.getElementById("sexo").value;
+
+    // envia os dados para o servidor
+    const response = await fetch('/', {
+        method: 'POST',
+        headers: {
+            // define o cabeçalho
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ nome, nascimento, celular, endereco, sexo }),
+    })
+
+    // captura a resposta
+    const data = await response.json();
+    console.log('cliente cadastrado', data);
+}
+
+
+
+// traz os veiculos
+async function carregarClientes() {
+    // captura os valores do form
+    try {
+        const response = await fetch('/');
+        const data = await response.json();
+
+        // captura a tabela
+        const tabelaClientes = document.getElementById('clientes');
+        // carrega as informacoes
+        tabelaClientes.innerHTML = '';
+
+        // cria uma linha para cada veiculo
+        data.forEach(cliente => {
+            const row = document.createElement('tr');
+            // adiciona as informações
+            row.innerHTML = `
+                <td>${cliente.nome}</td>
+                <td>${cliente.nascimento}</td>
+                <td>${cliente.celular}</td>
+                <td>${cliente.endereco}</td>
+                <td>${cliente.sexo}</td>
+            `;
+            // adiciona na tabela
+            tabelaClientes.appendChild(row);
+        });
+    } catch(error) {
+        console.error('Erro ao buscar cliente', error);
+    }
+}
