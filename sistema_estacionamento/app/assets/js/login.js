cec648b696b2b1c5052605c116b9ddb403c031c5
wesllen dhenmyz brito ferreira <156432084+wdhenmyz@users.noreply.github.com>
2024-08-13 10:04:04 -0300

    veiculos

--- sistema_estacionamento/app/assets/js/login.js
+++ sistema_estacionamento/app/assets/js/login.js
@@ -1,11 +1,12 @@
 document.getElementById('loginForm').addEventListener('submit', async (e) => {
     e.preventDefault();
 
+    const id = document.getElementById('id').value;
     const senha = document.getElementById('password').value;
     const usuario = document.getElementById('username').value;
     
     try {
-        const response = await fetch(`https://sheetdb.io/api/v1/mg07naffiti78/search?usuario=${usuario}&senha=${senha}`, {
+        const response = await fetch(`https://sheetdb.io/api/v1/mg07naffiti78/search?id=${id}&usuario=${usuario}&senha=${senha}`, {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
@@ -17,7 +18,8 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
         console.log(result);
 
         if (result.length > 0) {
-            window.location.href = 'index.html';
+            alert('Bem vindo');
+            window.location.href = 'index.html';           
         } else {
             alert('Usuário ou senha inválidos');
         }
