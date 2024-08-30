49ef8980fe4a87ef1528847389fb41f96773e7f1
wesllen dhenmyz brito ferreira <wesllen.dhenmyz@outlook.com>
2024-08-30 18:20:27 -0300

    Update login.js

--- sistema_estacionamento/app/assets/js/login.js
+++ sistema_estacionamento/app/assets/js/login.js
@@ -1,9 +1,10 @@
 document.getElementById('loginForm').addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    const id = document.getElementById('id').value;
     const senha = document.getElementById('password').value;
     const usuario = document.getElementById('username').value;
+    
+    
     
     try {
         const response = await fetch(`https://sheetdb.io/api/v1/mg07naffiti78/search?id=${id}&usuario=${usuario}&senha=${senha}`, {
@@ -18,9 +19,42 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
         console.log(result);
 
         if (result.length > 0) {
-            alert('Bem vindo');
-            window.location.href = 'index.html';     
-            return;      
+            async function confirm() {
+              let id = prompt("porfavor entre seu id:");
+
+              if (id == null || id.trim() === "") {
+                alert("Você não colocou um id válido");
+                return;
+              }
+
+              try {
+                const response = await fetch(`https://sheetdb.io/api/v1/mg07naffiti78/search?&id=${id}`, {
+                  method: 'GET',
+                  headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                  },
+                });
+
+                const result = await response.json();
+                console.log(result);
+
+                if (result.length > 0) {
+                    alert('Bem vindo');
+                    window.location.href = 'index.html';     
+                    return;  
+                  
+                } else {
+                  alert('Você não entrou um id válido');
+                }
+              } catch (error) {
+                console.error('Fetch error:', error);
+                alert('Ocorreu um erro ao buscar os dados. Tente novamente mais tarde.');
+              }
+            }
+
+            confirm()
+                
         } else {
             alert('Usuário ou senha inválidos');
         }
