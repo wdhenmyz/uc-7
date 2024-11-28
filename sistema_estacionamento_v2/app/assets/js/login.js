0d44b6a97723150f9414e0ff2ecc28d28812b19a
wdhenmyz <wesllen.dhenmyz@outlook.com>
2024-11-28 10:48:15 -0300

    app > assets > login

--- sistema_estacionamento_v2/app/assets/js/login.js
+++ sistema_estacionamento_v2/app/assets/js/login.js
@@ -1,4 +1,3 @@
-import { showPopup, hidePopup } from "../components/Popup.js";
 import { login } from "../components/login.js";
 
 // Evento de submissão do formulário de login
@@ -12,47 +11,3 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
 
   login(usuario, senha);
 });
-
-
-/*try {
-    // Exibir mensagem de carregamento
-    showPopup("Realizando login, aguarde......");
-
-    // Fazendo o GET com os parâmetros 'usuario' e 'senha' na URL
-    const response = await fetch(`http://localhost:3000/api/login?usuario=${encodeURIComponent(usuario)}&senha=${encodeURIComponent(senha)}`);
-
-    if (!response.ok) {
-      throw new Error("Erro ao buscar dados do usuário");
-    }
-
-    const result = await response.json();
-
-    if (result.success) {
-
-      // Exibir mensagem de sucesso e redirecionar após um curto intervalo
-      showPopup("Login com sucesso! Bem-vindo");
-      setTimeout(() => {
-        hidePopup();
-        window.location.href = 'index.html';
-      }, 1500); // Aguardar 1.5 segundos antes de redirecionar
-
-    } else {
-
-      showPopup("Usuário ou senha incorretos")
-
-      setTimeout(() => {
-        hidePopup();
-      }, 1500); // Aguardar 1.5 segundos antes de redirecionar
-
-    }
-  } catch (error) {
-
-    showPopup("Usuário ou senha incorretos")
-
-    setTimeout(() => {
-      hidePopup();
-    }, 1500); // Aguardar 1.5 segundos antes de redirecionar
-
-    console.error("Erro ao buscar o usuário:", error);
- 
-  }*/
